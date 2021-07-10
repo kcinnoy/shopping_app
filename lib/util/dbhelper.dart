b95e77c7e4a34a9449d9313fe68033356825a7fe
kcinnoy <yonnick00@gmail.com>
2021-07-10 18:09:50 +0100

    added main db and test db

--- lib/util/dbhelper.dart
+++ lib/util/dbhelper.dart
@@ -6,6 +6,17 @@ class DbHelper {
   final int version = 1;
   Database? db;
 
+  Future testDb() async {
+    db = await openDb();
+    await db!.execute('INSERT INTO lists VALUES (0, "Fruit", 2)');
+    await db!.execute('''INSERT INTO items VALUES (0, 0, "Apples",'
+        '"2 Kg", "Better if they are green")''');
+    List lists = await db!.rawQuery('select * from lists');
+    List items = await db!.rawQuery('select * from items');
+    print(lists[0].toString());
+    print(items[0].toString());
+  }
+
   Future<Database> openDb() async {
     if (db == null) {
       db = await openDatabase(join(await getDatabasesPath(), 'shopping.db'),
