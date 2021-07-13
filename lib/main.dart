a589517dbbad6f6d94069d070355ba68e1597c34
kcinnoy <yonnick00@gmail.com>
2021-07-13 08:48:34 +0100

    added stful class to display model db info

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,42 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    DbHelper helper = DbHelper();
-    helper.testDb();
-
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Shopping List'),
+        ),
+        body: ShList(),
+      ),
     );
+  }
+}
+
+class ShList extends StatefulWidget {
+  @override
+  _ShListState createState() => _ShListState();
+}
+
+class _ShListState extends State<ShList> {
+  DbHelper helper = DbHelper();
+
+  @override
+  Widget build(BuildContext context) {
+    showData();
+    return Container();
+  }
+
+  Future showData() async {
+    await helper.openDb();
+    ShoppingList list = ShoppingList(0, 'Bakery', 2);
+    int listId = await helper.insertList(list);
+    ListItem item = ListItem(0, listId, 'Bread', 'note', '1 kg');
+    int itemId = await helper.insertItem(item);
+    print('List Id: ' + listId.toString());
+    print('Item Id: ' + itemId.toString());
   }
 }
