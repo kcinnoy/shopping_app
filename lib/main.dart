3cd896a2cde8e5f03a2cfb973189750084713424
kcinnoy <yonnick00@gmail.com>
2021-07-14 18:03:09 +0100

    added CRU functionality

--- lib/main.dart
+++ lib/main.dart
@@ -14,17 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Shopping List'),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-        body: ShList(),
-      ),
-    );
+        home: ShList());
   }
 }
 
@@ -37,30 +31,58 @@ class _ShListState extends State<ShList> {
   DbHelper helper = DbHelper();
   List<ShoppingList>? shoppingList;
 
+  ShoppingListDialog? dialog;
+  @override
+  void initState() {
+    dialog = ShoppingListDialog();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     showData();
-    return ListView.builder(
-      itemCount: shoppingList?.length ?? 0,
-      itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          title: Text(shoppingList![index].name),
-          leading: CircleAvatar(
-            child: Text(shoppingList![index].priority.toString()),
-          ),
-          trailing: IconButton(
-            icon: Icon(Icons.edit),
-            onPressed: () {},
-          ),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => ItemsScreen(shoppingList![index])),
-            );
-          },
-        );
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shopping List'),
+      ),
+      body: ListView.builder(
+        itemCount: shoppingList?.length ?? 0,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(shoppingList![index].name),
+            leading: CircleAvatar(
+              child: Text(shoppingList![index].priority.toString()),
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) => dialog!
+                        .buildDialog(context, shoppingList![index], false));
+              },
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => ItemsScreen(shoppingList![index])),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) =>
+                dialog!.buildDialog(context, ShoppingList(0, '', 0), true),
+          );
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.pink,
+      ),
     );
   }
 
